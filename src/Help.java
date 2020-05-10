9972a3c3e11707195c3effcbab8305de3f0c3c87
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-05-10 01:03:52 -0400

    Update Help.java

--- src/Help.java
+++ src/Help.java
@@ -1,12 +1,13 @@
 public class Help {
 
     public static void display() {
-        System.out.println("help:                       Shows all available commands.");
-        System.out.println("exit:                       Closes the shell.");
-        System.out.println("lookup (symbol):            Looks up the information of an element.");
-        System.out.println("palookup (name):            Looks up the information of a polyatomic ion.");
-        System.out.println("mm (symbol):                Finds the molar mass of a compound, for exmaple: 1N 4H 1Cl.");
-        System.out.println("moles (symbol, grams):      Converts grams of a substance to moles, for example: 2H 1O, 198.2");
-        System.out.println("lr:                         Limiting reactant calculator between two compounds with mole ratios.");
+        System.out.println("help:                                           Shows all available commands.");
+        System.out.println("exit:                                           Closes the shell.");
+        System.out.println("lookup (symbol):                                Looks up the information of an element.");
+        System.out.println("palookup (name):                                Looks up the information of a polyatomic ion.");
+        System.out.println("mm (symbol):                                    Finds the molar mass of a compound, for exmaple: 1N 4H 1Cl.");
+        System.out.println("moles (symbol, grams):                          Converts grams of a substance to moles, for example: 2H 1O, 198.2");
+        System.out.println("lr:                                             Limiting reactant calculator between two compounds with mole ratios.");
+        System.out.println("se (energy change, mass, temperature change):   Calculates the specific heat of a substance.");
     }
 }
