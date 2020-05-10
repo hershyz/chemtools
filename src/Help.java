2268c25887d898fcb083ccbf4a1fc33c73d8ddef
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-05-10 00:15:40 -0400

    Update Help.java

--- src/Help.java
+++ src/Help.java
@@ -6,7 +6,7 @@ public class Help {
         System.out.println("lookup (symbol):            Looks up the information of an element.");
         System.out.println("palookup (name):            Looks up the information of a polyatomic ion.");
         System.out.println("mm (symbol):                Finds the molar mass of a compound, for exmaple: 1N 4H 1Cl.");
-        System.out.println("moles (symbol, grams):      Converts grams of a substance to moles, for example: 2H 1O, ");
+        System.out.println("moles (symbol, grams):      Converts grams of a substance to moles, for example: 2H 1O, 198.2");
         System.out.println("lr:                         Limiting reactant calculator between two compounds with mole ratios.");
     }
 }
